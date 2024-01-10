1df1abd917e06be44b57c012772a0bb17705f055
shibotgit <helloakaa1@gmail.com>
2024-01-10 15:04:57 +0000

    version

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,5 +4,5 @@ bind = "0.0.0.0:8080"
 workers = multiprocessing.cpu_count() * 2 + 1  # Adjust the number of workers based on your server's resources
 worker_class = "gevent"
 worker_connections = 1000
-timeout = 30
+timeout = 360
 keepalive = 2
